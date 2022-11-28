f9da4ebff0d486ddbadb1a4bc71ef6095489cfc9
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2022-11-28 15:16:10 +0000

    done 1-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('form.login-form');
 
+formRef.addEventListener('submit', handleSubmitForm)
+
+function handleSubmitForm(evt) {
+  evt.preventDefault();
+
+  const {email, password} = evt.currentTarget.elements;
+
+  if (email.value === '' || password.value === '') {
+    alert('All fields must be filled');
+    return;
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  }
+
+  console.log(formData)
+  formRef.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeButton = document.querySelector('button.change-color');
+const textColor =  document.querySelector('span.color');
+console.log(changeButton)
+
+
+changeButton.addEventListener('click', function() {
+  const colorBg = getRandomHexColor();
+  document.body.style.background = colorBg;
+  textColor.textContent = colorBg;
+})
